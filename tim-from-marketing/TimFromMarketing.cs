e01b5e50d6cb6433805e4813f2507aaf95b01a85
EllieD33 <120728802+EllieD33@users.noreply.github.com>
2024-07-30 17:01:18 +0100

    completes TimFromMarketing

---
+++ tim-from-marketing/TimFromMarketing.cs
@@ -0,0 +1,12 @@
+using System;
+
+static class Badge
+{
+    public static string Print(int? id, string name, string? department)
+    {   
+        string idString = id == null ? "" : $"[{id}] - ";
+        string departmentString = department == null ? "OWNER" :  $"{department.ToUpper()}";
+
+        return idString + name + " - " + departmentString;
+    }
+}
